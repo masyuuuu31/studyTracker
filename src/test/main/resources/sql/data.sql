2fc6505abbc23df303ca3f2e79227f8877d18178
kousei <k.saito10114@gmail.com>
2024-12-09 23:00:25 +0900

    SQLの修正

--- src/test/main/resources/sql/data.sql
+++ src/test/main/resources/sql/data.sql
@@ -1,36 +1,47 @@
-INSERT INTO m_user ( user_id,user_name,password,created_at,updated_at ) 
-VALUES ( "k.saito10114@gmail.com","saito","saitokosei","2024-12-07",'2024-12-08' );
+CREATE DATABASE studyTracker;
+USE studyTracker;
 
-INSERT INTO m_user ( user_id,user_name,password,created_at,updated_at ) 
-VALUES ( "yuutakaizyuu2@gmail.com","nishizawa","nishizawayuta",'2024-12-07','2024-12-08' );
+CREATE TABLE IF NOT EXISTS m_user (
+    user_id VARCHAR(256) PRIMARY KEY,
+    user_name VARCHAR(256) NOT NULL,
+    password VARCHAR(256) NOT NULL,
+    authority SMALLINT NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
 
-INSERT INTO m_user ( user_id,user_name,password,created_at,updated_at ) 
-VALUES ( "inoueritsu131@gmail.com","inoue","inoueritsu",'2024-12-07','2024-12-08' );
+CREATE TABLE IF NOT EXISTS main_task (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(256) NOT NULL,
+    description TEXT,
+    deadline TIMESTAMP,
+    percent DOUBLE,
+    total_time DOUBLE,
+    author_id VARCHAR(256),
+    status SMALLINT,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    FOREIGN KEY (author_id) REFERENCES m_user(user_id)
+);
 
-INSERT INTO m_user ( user_id,user_name,password,created_at,updated_at ) 
-VALUES ( "test@example.com","test","test_example",'2024-12-07','2024-12-08' );
+CREATE TABLE IF NOT EXISTS sub_task (
+     id BIGINT PRIMARY KEY AUTO_INCREMENT,
+     title VARCHAR(256) NOT NULL,
+     description TEXT,
+     target_id BIGINT,
+     author_id VARCHAR(256),
+     started_at TIMESTAMP,
+     finished_at TIMESTAMP,
+     total_time DOUBLE,
+     status SMALLINT,
+     created_at TIMESTAMP,
+     updated_at TIMESTAMP,
+     FOREIGN KEY (author_id) REFERENCES m_user(user_id),
+     FOREIGN KEY (target_id) REFERENCES main_task(id)
+);
 
-メインタスク
-INSERT INTO main_task ( title, description, deadline, percent, total_time, author_id, status, created_at, updated_at ) 
-VALUES ( "SQLマスター", "SQL構文の習得", '2024-12-31', 0, 0, "k.saito10114@gmail.com", 0, '2024-12-07', '2024-12-07' );
-
-INSERT INTO main_task ( title, description, deadline, percent, total_time, author_id, status, created_at, updated_at ) 
-VALUES ( "海賊王に俺はなる！", "この世界の海で一番自由な男", '2024-12-31', 0, 0, "yuutakaizyuu2@gmail.com", 0, '2024-12-07', '2024-12-07' );
-
-INSERT INTO main_task ( title, description, deadline, percent, total_time, author_id, status, created_at, updated_at ) 
-VALUES ( "痩せる", "目指せ体重20kg減", '2024-12-31', 0, 0, "inoueritsu131@gmail.com", 0, '2024-12-07', '2024-12-07' );
-
-
-サブタスク
-INSERT INTO sub_task (title,description,target_id,author_id,started_at,finished_at,total_time,status,created_at,updated_at )
-VALUES ( "SQL1","SQLの習得1",1,"k.saito10114@gmail.com",'2024-12-07-11:00','2024-12-07-20:00',0,0,'2024-12-07','2024-12-07' );
-
-INSERT INTO sub_task (title,description,target_id,author_id,started_at,finished_at,total_time,status,created_at,updated_at )
-VALUES ( "SQL1","SQLの習得1",1,"k.saito10114@gmail.com",'2024-12-08-10:00','2024-12-08-19:00',0,0,'2024-12-07','2024-12-07' );
-
-INSERT INTO sub_task (title,description,target_id,author_id,started_at,finished_at,total_time,status,created_at,updated_at )
-VALUES ( "SQL2","SQLの習得2",1,"k.saito10114@gmail.com",'2024-12-09-08:30','2024-12-09-17:30',0,0,'2024-12-08','2024-12-08' );
-
-INSERT INTO sub_task (title,description,target_id,author_id,started_at,finished_at,total_time,status,created_at,updated_at )
-VALUES ( "SQL2","SQLの習得2",1,"k.saito10114@gmail.com",'2024-12-09-08:30','2024-12-09-17:30',0,0,'2024-12-08','2024-12-08' );
-
+INSERT INTO m_user ( user_id,user_name,password,created_at,updated_at,)
+     VALUES ( "k.saito10114@gmail.com","Kosei","SaitoKosei",,NOW(),NOW()),
+            ( "yuutakaizyuu2@gmail.com","Yuta","NishizawaYuta",,NOW(),NOW()),
+            ( "inoueritsu131@gmail.com","Ritsu","InoueRitsu",,NOW(),NOW()
+);
